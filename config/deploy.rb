b06ba07ccf601b238bfaa6ab2cdec15d2a16c946
Kenneth Lim <kennethlimjf@gmail.com>
2014-12-25 01:13:37 +0800

    Updated deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,8 +11,7 @@ set :rbenv_roles, :all
 
 set :deploy_to, '/data/www/sentwix'
 
-# Default value for :linked_files is []
-# set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml .env}
 
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
