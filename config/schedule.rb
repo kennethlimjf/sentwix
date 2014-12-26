5aa81453ea95cbed286b449e4be9884be41cb1fd
Kenneth Lim <kennethlimjf@gmail.com>
2014-12-27 00:24:57 +0800

    crontab

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,5 @@
+set :output, "#{path}/../../shared/log/cron.log"
+
 every 1.minute do
-  command "source ~/.bashrc; rbenv shell 2.1.5; cd /data/www/sentwix/current && RAILS_ENV=production bundle exec rake analysis --silent >> /data/www/sentwix/shared/log/cron.log 2>&1"
+  rake 'analysis'
 end
