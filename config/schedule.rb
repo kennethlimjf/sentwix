f97131eca76ce39f0ceec5780d69120d99fed9db
Kenneth Lim <kennethlimjf@gmail.com>
2014-12-26 23:29:28 +0800

    changed whenever to use command

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,3 @@
-set :output, "#{path}/../shared/log/cron.log"
-
 every 1.minute do
-  rake 'analysis'
+  command "source ~/.bashrc; rbenv shell 2.1.5; cd /data/www/sentwix/current && RAILS_ENV=production bundle exec rake analysis --silent >> /data/www/sentwix/shared/log/cron.log 2>&1"
 end
